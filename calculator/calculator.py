c997eb74274b92d8fef4cd0b88e2a45c40639017
Yes526-tech <ysert20051@gmail.com>
2021-06-13 17:08:33 +0300

    finish project added

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,30 +1,37 @@
-# def add(n1, n2):
-#     return n1 + n2
-# def subtract(n1, n2):
-#     return n1 - n2
-# def multiply(n1, n2):
-#     return n1 * n2
-# def divide(n1, n2):
-#     return n1 / n2
-#
-# operator = {
-# "+": add,
-# "-": subtract,
-# "*": multiply,
-# "/": divide
-# }
-#
-# num1 = int(input("number 1>"))
-# for sembol in operator:
-#     print(sembol)
-# operator_Select = input("which operator:")
-# selected_operator = operator[operator_Select]
-# num2 = int(input("number 2>"))
-# answer = selected_operator(num1, num2)
-# print(f"{num1} {operator_Select} {num2} = {answer}")
-# operator_Select = input("which operator:")
-# selected_operator = operator[operator_Select]
-# num3 = int(input("number 3>"))
-# second_answer = selected_operator(answer, num3)
-# print(f"{answer} {operator_Select} {num3} = {second_answer}")
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+operator = {
+"+": add,
+"-": subtract,
+"*": multiply,
+"/": divide
+}
+def calculator():
+    num1 = float(input("number 1>"))
+    for symbol in operator:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operator_Select = input("which operator:")
+        selected_operator = operator[operator_Select]
+        num2 = float(input("number 2>"))
+        answer = selected_operator(num1, num2)
+
+        print(f"{num1} {operator_Select} {num2} = {answer}")
+
+        if input(f"type to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
 
