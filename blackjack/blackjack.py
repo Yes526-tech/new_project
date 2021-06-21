4b49e4b38656caaddff092399a3a0c3bcb5a4057
Yes526-tech <ysert20051@gmail.com>
2021-06-21 18:26:24 +0300

    project added

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -30,7 +30,8 @@ def compare(user_score, computer_score):
 user_cards = []
 computer_cards = []
 is_game_over = False
-
+computer_score = calculate_score(computer_cards)
+user_score = calculate_score(user_cards)
 for _ in range(2):
     new_card = deal_card()
     user_cards.append(new_card)
@@ -41,7 +42,7 @@ while not is_game_over:
     print(f"your cards: {user_cards}, current score: {user_score}")
     print(f"computer cards: {computer_cards}, current score: {computer_score}")
 
-    if user_score == 0 or computer_score == 0 or user_score > 21:
+    if user_score == 0 or computer_score == 0 or user_score > 21 or computer_score > 21:
         is_game_over = True
     else:
         user_should_deal = input("type 'y' to get another card, type 'n' to pass: ")
@@ -49,7 +50,11 @@ while not is_game_over:
             user_cards.append(deal_card())
         else:
             is_game_over = True
-while computer_score != 0 and computer_score < 17:
+
+
+while computer_score != 0 and computer_score < 17 and not user_score > 21:
     computer_cards.append(deal_card())
     computer_score = calculate_score(computer_cards)
+print(f"    Your final hand: {user_cards}, final score: {user_score}")
+print(f"    Computer final hand: {computer_cards}, final score: {computer_score}")
 print(compare(user_score, computer_score))
