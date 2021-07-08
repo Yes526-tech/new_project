6922c869b33b4f4d15f13226cedee70e69979356
Yes526-tech <ysert20051@gmail.com>
2021-07-08 11:53:39 +0300

    drawing

---
+++ class an OOP/blueprint.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+my_screen = Screen()
+timmy.shape("turtle")
+timmy.color("red")
+
+timmy.forward(100)
+timmy.left(100)
+timmy.tilt(90)
+
+timmy.forward(100)
+
+
+timmy.left(100)
+timmy.forward(100)
+
+
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+
+
+my_screen.exitonclick()
